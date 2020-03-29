10742cc49a95340cbdf01cb271d307aed4091208
LeAnhdung <jlehoola@yahoo.com>
2020-03-28 23:28:48 -0700

    1st

---
+++ mar28_2/src/Hello.java
@@ -0,0 +1,16 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		String[] names = {"AA", "CC",  "EE","FF"};
+  
+		for (String name : names) {
+			System.out.format("Hello, %!\n", name);
+			System.out.format("How are u doing today?");
+ 
+	}
+
+}
+}
